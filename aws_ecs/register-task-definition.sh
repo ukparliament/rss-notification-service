14d19d438bf5c3c0180211306594df4531d47f95
Sarah Allett <alletts@parliament.uk>
2018-10-30 13:20:54 +0000

    pipeline files

---
+++ aws_ecs/register-task-definition.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+set -e
+
+APP_NAME=$1
+
+TMPFILE=$(mktemp)
+aws ecs describe-task-definition --task-definition $APP_NAME | jq "{ containerDefinitions: .taskDefinition.containerDefinitions, family: .taskDefinition.family }" > $TMPFILE
+aws ecs register-task-definition --cli-input-json file://$TMPFILE
+rm $TMPFILE
