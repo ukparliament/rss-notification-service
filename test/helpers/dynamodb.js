ae9cf722d4f756abb914bfddd5c0fc3f4ae523c9
Jake Mulley <mulleyjb@parliament.uk>
2018-11-02 11:54:11 +0000

    Implement SES interface

--- test/helpers/dynamodb.js
+++
@@ -1,8 +0,0 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'local', accessKeyId: 'notARealAccessId', secretAccessKey: 'notARealSecretKey' });
-
-module.exports = new AWS.DynamoDB({
-  endpoint: new AWS.Endpoint(process.env.AWS_DYNAMODB_ENDPOINT || 'http://localhost:8000'),
-  apiVersion: '2012-08-10',
-  convertEmptyValues: true
-});
